b632a055d5d112f47ce0be423d411b023ffbfcd5
Zac <zbenattar@gmail.com>
2025-02-07 11:20:26 +0000

    Add sign in sign out components

---
+++ src/app/_components/sign-out-button.tsx
@@ -0,0 +1,22 @@
+import { MdOutlineExitToApp } from "react-icons/md";
+import { signOut } from "~/server/auth";
+
+export default function SignOutButton() {
+  return (
+    <button
+      type="submit"
+      onClick={async () => {
+        "use server";
+
+        await signOut({ redirectTo: "/" });
+      }}
+    >
+      <div className="flex flex-row place-content-start gap-2">
+        <div className="flex flex-col place-content-center">
+          <MdOutlineExitToApp />
+        </div>
+        Sign Out
+      </div>
+    </button>
+  );
+}

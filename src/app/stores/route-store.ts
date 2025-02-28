3d1c9790abb5b04c3ac0b4fe48b02d4cea3d384f
Zac <zbenattar@gmail.com>
2025-02-28 09:17:44 +0000

    Add route state to url

--- src/app/stores/route-store.ts
+++ src/app/stores/route-store.ts
@@ -1,10 +1,60 @@
-import { create, type ExtractState } from "zustand";
-import { Waypoint } from "~/lib/types/waypoint";
+/*
+  This store is used to manage the waypoints and the route data.
+  It uses zustand for state management and zustand/middleware for persistence.
+  The store is persisted in local storage and the URL query params.
+  This allows for the state to be maintained across page reloads and shared via URL.
+  The store provides functions to add, remove, and move waypoints, as well as set the distance unit and max flight level.
+  The store also calculates the distance between waypoints and updates the distance display unit.
+*/
+
+import { create } from "zustand";
+import {
+  persist,
+  type StateStorage,
+  createJSONStorage,
+} from "zustand/middleware";
+import { type Waypoint } from "~/lib/types/waypoint";
 import * as turf from "@turf/turf";
 
-export type RouteSlice = ExtractState<typeof useRouteStore>;
+const getUrlSearch = () => {
+  return window.location.search.slice(1);
+};
 
-interface RouteState {
+const persistentStorage: StateStorage = {
+  getItem: (key): string => {
+    // Check URL first
+    if (getUrlSearch()) {
+      const searchParams = new URLSearchParams(getUrlSearch());
+      const storedValue = searchParams.get(key);
+      return JSON.parse(storedValue!) as string;
+    } else {
+      // Otherwise, we should load from localstorage or alternative storage
+      return JSON.parse(localStorage.getItem(key)!) as string;
+    }
+  },
+  setItem: (key, newValue): void => {
+    // Check if query params exist at all, can remove check if always want to set URL
+    if (getUrlSearch()) {
+      const searchParams = new URLSearchParams(getUrlSearch());
+      searchParams.set(key, JSON.stringify(newValue));
+      window.history.replaceState(null, "", `?${searchParams.toString()}`);
+    }
+
+    localStorage.setItem(key, JSON.stringify(newValue));
+  },
+  removeItem: (key): void => {
+    const searchParams = new URLSearchParams(getUrlSearch());
+    searchParams.delete(key);
+    window.location.search = searchParams.toString();
+  },
+};
+
+const storageOptions = {
+  name: "routeStore",
+  storage: createJSONStorage<RouteStore>(() => persistentStorage),
+};
+
+interface RouteStore {
   waypoints: Waypoint[];
   distanceKM: number;
   distanceDisplayUnit: string;
@@ -18,44 +68,50 @@ interface RouteState {
   setMaxFL: (maxFL: number) => void;
 }
 
-const useRouteStore = create<RouteState>()((set) => ({
-  waypoints: [],
-  distanceKM: 0,
-  distanceDisplayUnit: "nm",
-  maxFL: 30,
-  setWaypoints: (_waypoints: Waypoint[]) =>
-    set(() => ({ waypoints: _waypoints })),
-  moveWaypoint: (waypointId: string, newLocation: [number, number]) => {
-    set((state) => ({
-      waypoints: state.waypoints.map((waypoint) =>
-        waypoint.id === waypointId
-          ? { ...waypoint, location: newLocation }
-          : waypoint,
-      ),
-    }));
-    set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
-  },
-  addWaypoint: (waypoint: Waypoint) => {
-    set((state) => ({
-      waypoints: addWaypoint(waypoint, state.waypoints),
-    }));
-    set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
-  },
-  removeWaypoint: (waypointId: string) => {
-    set((state) => ({
-      waypoints: removeWaypoint(waypointId, state.waypoints),
-    }));
-    set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
-  },
-  swapWaypoints: (waypointA: Waypoint, waypointB: Waypoint) => {
-    set((state) => ({
-      waypoints: swapWaypoints(waypointA, waypointB, state.waypoints),
-    }));
-    set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
-  },
-  setDistanceUnit: (unit: string) => set(() => ({ distanceDisplayUnit: unit })),
-  setMaxFL: (maxFL: number) => set(() => ({ maxFL })),
-}));
+const useRouteStore = create(
+  persist<RouteStore>(
+    (set) => ({
+      waypoints: [],
+      distanceKM: 0,
+      distanceDisplayUnit: "nm",
+      maxFL: 30,
+      setWaypoints: (_waypoints: Waypoint[]) =>
+        set(() => ({ waypoints: _waypoints })),
+      moveWaypoint: (waypointId: string, newLocation: [number, number]) => {
+        set((state) => ({
+          waypoints: state.waypoints.map((waypoint) =>
+            waypoint.id === waypointId
+              ? { ...waypoint, location: newLocation }
+              : waypoint,
+          ),
+        }));
+        set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
+      },
+      addWaypoint: (waypoint: Waypoint) => {
+        set((state) => ({
+          waypoints: addWaypoint(waypoint, state.waypoints),
+        }));
+        set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
+      },
+      removeWaypoint: (waypointId: string) => {
+        set((state) => ({
+          waypoints: removeWaypoint(waypointId, state.waypoints),
+        }));
+        set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
+      },
+      swapWaypoints: (waypointA: Waypoint, waypointB: Waypoint) => {
+        set((state) => ({
+          waypoints: swapWaypoints(waypointA, waypointB, state.waypoints),
+        }));
+        set((state) => ({ distanceKM: updateDistance(state.waypoints) }));
+      },
+      setDistanceUnit: (unit: string) =>
+        set(() => ({ distanceDisplayUnit: unit })),
+      setMaxFL: (maxFL: number) => set(() => ({ maxFL })),
+    }),
+    storageOptions,
+  ),
+);
 
 export default useRouteStore;
 
